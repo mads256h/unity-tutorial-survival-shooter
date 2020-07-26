957ccb980c7108d473056212e6341486b3f5c06e
Mads Mogensen <mail@madsmogensen.dk>
2020-07-26 12:31:54 +0200

    A lot of shit idk

--- Assets/Scripts/BuyMenu/BuyMenuItem.cs
+++ Assets/Scripts/BuyMenu/BuyMenuItem.cs
@@ -1,60 +1,64 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using BuyMenu;
-using TMPro;
+﻿using TMPro;
 using UnityEngine;
-using UnityEngine.Serialization;
 using UnityEngine.UI;
 
-public class BuyMenuItem : MonoBehaviour
+namespace BuyMenu
 {
-    [SerializeField] private TextMeshProUGUI title;
-    [SerializeField] private TextMeshProUGUI price;
-
-    [SerializeField] private Button button;
-    
-    [SerializeField] private Image[] images;
-
-    [SerializeField] private Sprite activatedSprite;
-    
-    private BuyMenuItemProperties _properties;
-
-    private int _currentPrice = 0;
-
-    private int _level = 0;
-    
-
-    public void Initialize(BuyMenuItemProperties properties)
+    public class BuyMenuItem : MonoBehaviour
     {
-        _properties = properties;
-    }
-    
-    // Start is called before the first frame update
-    private void Start()
-    {
-        title.text = _properties.Title;
-        _currentPrice = _properties.BasePrice;
-        price.text = "$" + _currentPrice;
-    }
+        [SerializeField] private TextMeshProUGUI title;
+        [SerializeField] private TextMeshProUGUI price;
 
-    public void OnButtonPress()
-    {
-        images[_level].sprite = activatedSprite;
-        ScoreManager.score -= _currentPrice;
-        _level++;
-        _properties.OnPurchase(_level);
-        UpdatePrice();
-    }
-
-    private void UpdatePrice()
-    {
-        _currentPrice = (int)((float)_currentPrice * _properties.PriceMultiplier);
-        price.text = "$" + _currentPrice;
-    }
+        [SerializeField] private Button button;
     
-    // Update is called once per frame
-    private void Update()
-    {
-        button.interactable = ScoreManager.score >= _currentPrice;
+        [SerializeField] private Image[] images;
+
+        [SerializeField] private Sprite activatedSprite;
+    
+        private BuyMenuItemProperties _properties;
+
+        private int _currentPrice = 0;
+
+        private int _level = 0;
+    
+
+        public void Initialize(BuyMenuItemProperties properties)
+        {
+            _properties = properties;
+        }
+    
+        // Start is called before the first frame update
+        private void Start()
+        {
+            title.text = _properties.Title;
+            _currentPrice = _properties.BasePrice;
+            price.text = "$" + _currentPrice;
+        }
+
+        public void OnButtonPress()
+        {
+            images[_level].sprite = activatedSprite;
+            ScoreManager.score -= _currentPrice;
+            _level++;
+            _properties.OnPurchase(_level);
+            UpdatePrice();
+
+            // Disable script when we hit level 10
+            if (_level != 10) return;
+            enabled = false;
+            button.interactable = false;
+        }
+
+        private void UpdatePrice()
+        {
+            _currentPrice = (int)((float)_currentPrice * _properties.PriceMultiplier);
+            price.text = "$" + _currentPrice;
+        }
+    
+        // Update is called once per frame
+        private void Update()
+        {
+            button.interactable = ScoreManager.score >= _currentPrice;
+        }
     }
 }
--- Assets/_CompletedAssets/Scripts/Player/PlayerShooting.cs
+++ Assets/_CompletedAssets/Scripts/Player/PlayerShooting.cs
@@ -5,69 +5,91 @@ namespace CompleteProject
 {
     public class PlayerShooting : MonoBehaviour
     {
-        public int damagePerShot = 20;                  // The damage inflicted by each bullet.
-        public float timeBetweenBullets = 0.15f;        // The time between each shot.
-        public float range = 100f;                      // The distance the gun can fire.
+        [SerializeField] private int damagePerShot = 20;                  // The damage inflicted by each bullet.
+        [SerializeField] private float timeBetweenBullets = 0.15f;        // The time between each shot.
+        [SerializeField] private float range = 100f;                      // The distance the gun can fire.
+        [SerializeField] private float effectsDisplayTime = 0.2f;                // The proportion of the timeBetweenBullets that the effects will display for.
 
-
-        float timer;                                    // A timer to determine when to fire.
-        Ray shootRay = new Ray();                       // A ray from the gun end forwards.
-        RaycastHit shootHit;                            // A raycast hit to get information about what was hit.
-        int shootableMask;                              // A layer mask so the raycast only hits things on the shootable layer.
-        ParticleSystem gunParticles;                    // Reference to the particle system.
-        LineRenderer gunLine;                           // Reference to the line renderer.
-        AudioSource gunAudio;                           // Reference to the audio source.
-        Light gunLight;                                 // Reference to the light component.
-		public Light faceLight;								// Duh
-        float effectsDisplayTime = 0.2f;                // The proportion of the timeBetweenBullets that the effects will display for.
-
-        [SerializeField] private Material gunLineLevel5;
-        [SerializeField] private Material gunLineLevel10;
-
-        [SerializeField] private GameObject grenade;
-
+        
         [SerializeField] private float grenadeSpeed = 500.0f;
         [SerializeField] private float playerGrenadeSpeed = 10.0f;
+        [SerializeField] private float timeBetweenGrenades = 10.0f;
 
+
+        [SerializeField] private LayerMask shootableMask;
+        
+        [SerializeField] private Light faceLight;								// Duh
+        
+        [SerializeField] private Material gunLineLevel5;
+        [SerializeField] private Material gunLineLevel10;
+        
+        
         [SerializeField] private Rigidbody playerRigidbody;
+        [SerializeField] private GameObject grenade;
+        
+        
+        private int _grenadeDamage = 20;
         
         private Transform _transform;
 
         private Vector3 _lastPos;
 
         private float _grenadeTimer = 0.0f;
+        private int _grenadeLevel = 0;
         
-        public void UpgradeFirerate(int level)
+        private float _gunTimer;                                    // A timer to determine when to fire.
+        private Ray _shootRay = new Ray();                       // A ray from the gun end forwards.
+        private RaycastHit _shootHit;                            // A raycast hit to get information about what was hit.
+        private int _shootableMask;                              // A layer mask so the raycast only hits things on the shootable layer.
+        private ParticleSystem _gunParticles;                    // Reference to the particle system.
+        private LineRenderer _gunLine;                           // Reference to the line renderer.
+        private AudioSource _gunAudio;                           // Reference to the audio source.
+        private Light _gunLight;                                 // Reference to the light component. 
+        
+        public void UpgradeGunFirerate(int level)
         {
             timeBetweenBullets /= 2.0f;
         }
 
-        public void UpgradeDamage(int level)
+        public void UpgradeGunDamage(int level)
         {
             damagePerShot *= 2;
             switch (level)
             {
                 case 5:
-                    gunLine.material = gunLineLevel5;
-                    gunLight.color = faceLight.color = Color.red;
+                    _gunLine.material = gunLineLevel5;
+                    _gunLight.color = faceLight.color = Color.red;
                     break;
                 case 10:
-                    gunLine.material = gunLineLevel10;
-                    gunLight.color = faceLight.color = Color.blue;
+                    _gunLine.material = gunLineLevel10;
+                    _gunLight.color = faceLight.color = Color.blue;
                     break;
             }
         }
 
-        void Awake ()
+        public void UpgradeGrenadeFirerate(int level)
+        {
+            timeBetweenGrenades /= 2.0f;
+            
+            Debug.Log("Upgraded grenade!");
+        }
+
+        public void UpgradeGrenadeDamage(int level)
+        {
+            _grenadeDamage *= 2;
+            _grenadeLevel = level;
+        }
+
+        private void Awake ()
         {
             // Create a layer mask for the Shootable layer.
-            shootableMask = LayerMask.GetMask ("Shootable");
+            _shootableMask = shootableMask.value;
 
             // Set up the references.
-            gunParticles = GetComponent<ParticleSystem> ();
-            gunLine = GetComponent <LineRenderer> ();
-            gunAudio = GetComponent<AudioSource> ();
-            gunLight = GetComponent<Light> ();
+            _gunParticles = GetComponent<ParticleSystem> ();
+            _gunLine = GetComponent <LineRenderer> ();
+            _gunAudio = GetComponent<AudioSource> ();
+            _gunLight = GetComponent<Light> ();
 			//faceLight = GetComponentInChildren<Light> ();
 
             _transform = GetComponent<Transform>();
@@ -76,21 +98,21 @@ namespace CompleteProject
         }
 
 
-        void Update ()
+        private void Update ()
         {
             // Add the time since Update was last called to the timer.
-            timer += Time.deltaTime;
+            _gunTimer += Time.deltaTime;
             _grenadeTimer += Time.deltaTime;
 
 #if !MOBILE_INPUT
             // If the Fire1 button is being press and it's time to fire...
-			if(Input.GetButton ("Fire1") && timer >= timeBetweenBullets && Time.timeScale != 0)
+			if(Input.GetButton ("Fire1") && _gunTimer >= timeBetweenBullets && Time.timeScale != 0)
             {
                 // ... shoot the gun.
                 Shoot ();
             }
 
-            if (Input.GetButton("Fire2") && _grenadeTimer >= timeBetweenBullets) 
+            if (Input.GetButton("Fire2") && _grenadeTimer >= timeBetweenGrenades) 
             {
                 ThrowGrenade();
             }
@@ -103,7 +125,7 @@ namespace CompleteProject
             }
 #endif
             // If the timer has exceeded the proportion of timeBetweenBullets that the effects should be displayed for...
-            if(timer >= timeBetweenBullets * effectsDisplayTime)
+            if(_gunTimer >= timeBetweenBullets * effectsDisplayTime)
             {
                 // ... disable the effects.
                 DisableEffects ();
@@ -116,73 +138,79 @@ namespace CompleteProject
         public void DisableEffects ()
         {
             // Disable the line renderer and the light.
-            gunLine.enabled = false;
+            _gunLine.enabled = false;
 			faceLight.enabled = false;
-            gunLight.enabled = false;
+            _gunLight.enabled = false;
         }
 
 
-        void ThrowGrenade()
+        private void ThrowGrenade()
         {
             _grenadeTimer = 0.0f;
             
             var g = Instantiate(grenade, _transform.position, _transform.rotation);
             var rigid = g.GetComponent<Rigidbody>();
+            var gs = g.GetComponent<Grenade>();
+
+            gs.Initialize(_grenadeDamage, _grenadeLevel);
 
 
             var playerVelocity = _lastPos - playerRigidbody.position;
             
-            rigid.velocity = -(playerVelocity * playerGrenadeSpeed);
-            rigid.velocity += (g.transform.forward * grenadeSpeed);
-            
-            Debug.Log(rigid.velocity);
+            rigid.velocity = (-(playerVelocity * playerGrenadeSpeed)) + (g.transform.forward * grenadeSpeed);
+
         }
         
-        void Shoot ()
+        private void Shoot ()
         {
             // Reset the timer.
-            timer = 0f;
+            _gunTimer = 0f;
 
             // Play the gun shot audioclip.
-            gunAudio.Play ();
+            _gunAudio.Play ();
 
             // Enable the lights.
-            gunLight.enabled = true;
+            _gunLight.enabled = true;
 			faceLight.enabled = true;
 
             // Stop the particles from playing if they were, then start the particles.
-            gunParticles.Stop ();
-            gunParticles.Play ();
+            _gunParticles.Stop ();
+            _gunParticles.Play ();
 
             // Enable the line renderer and set it's first position to be the end of the gun.
-            gunLine.enabled = true;
-            gunLine.SetPosition (0, transform.position);
+            _gunLine.enabled = true;
+            _gunLine.SetPosition (0, transform.position);
 
             // Set the shootRay so that it starts at the end of the gun and points forward from the barrel.
-            shootRay.origin = _transform.position;
-            shootRay.direction = _transform.forward;
-
+            _shootRay.origin = _transform.position;
+            _shootRay.direction = _transform.forward;
+            
+            
             // Perform the raycast against gameobjects on the shootable layer and if it hits something...
-            if(Physics.Raycast (shootRay, out shootHit, range, shootableMask))
+            if(Physics.Raycast (_shootRay, out _shootHit, range, _shootableMask))
             {
+                //LayerMask.
+                
+                //_shootHit.collider.gameObject.layer.
+                
                 // Try and find an EnemyHealth script on the gameobject hit.
-                EnemyHealth enemyHealth = shootHit.collider.GetComponent <EnemyHealth> ();
+                var enemyHealth = _shootHit.collider.GetComponent<EnemyHealth>();
 
                 // If the EnemyHealth component exist...
                 if(enemyHealth != null)
                 {
                     // ... the enemy should take damage.
-                    enemyHealth.TakeDamage (damagePerShot, shootHit.point);
+                    enemyHealth.TakeDamage (damagePerShot, _shootHit.point);
                 }
 
                 // Set the second position of the line renderer to the point the raycast hit.
-                gunLine.SetPosition (1, shootHit.point);
+                _gunLine.SetPosition (1, _shootHit.point);
             }
             // If the raycast didn't hit anything on the shootable layer...
             else
             {
                 // ... set the second position of the line renderer to the fullest extent of the gun's range.
-                gunLine.SetPosition (1, shootRay.origin + shootRay.direction * range);
+                _gunLine.SetPosition (1, _shootRay.origin + _shootRay.direction * range);
             }
         }
     }
